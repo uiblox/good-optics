cebd3828040f6a3c283492b574116e76f9cb88ee
Christian <bournecreative@gmail.com>
2025-01-25 17:38:37 -0600

    Understand intersectional observer - how to maintain intersections when target is fully viewable and when target intersects the middle of the viewport

--- src/main.js
+++ src/main.js
@@ -1,59 +1,37 @@
-import './style.css'
-import "./styles.scss"
-import throttle from 'lodash.throttle'
+import "./styles.scss";
 
-// const parallaxTarget = document.querySelectorAll('.tsw-parallax')
-// console.log(parallaxTarget)
+// Here we are using intersectional observer to identify when our target is in view
+window.onload = () => {
+    const targetElement = document.querySelector('.test-subject');
+   
+    // Intersection will occur when target intersects the middle of the page
+    // const observer = new IntersectionObserver(getLocation, { root: null, threshold: 0, rootMargin: '-50% 0% -50% 0%' });
 
-// const calculateRangeValue = (oldMin, oldMax, newMin, newMax, oldValue) => {
-//   const oldRange = oldMax - oldMin;
-//   const newRange = newMax - newMin;
+    // Intersection will occur when entire target is in view
+    const observer = new IntersectionObserver(getLocation, { root: null, threshold: 1 });
+    function getLocation(entry) {
+        if (entry[0].isIntersecting) {
+            entry[0].target.style.backgroundColor = "red"
+        } else {
+            entry[0].target.style.backgroundColor = "cyan"
+        }
+    }
+    observer.observe(targetElement); 
+}
 
-//   return ((oldValue - oldMin) * newRange/oldRange) + newMin
-// }
 
-// function calcParallax () {
-//   const scrollTop = window.scrollY;
-//   parallaxTarget.forEach(target => {
-//     const heroText = target.nextElementSibling
-//     if ((scrollTop + window.innerHeight / 2) > target.offsetTop) {
-//       const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
-//       const oldMax = oldMin + target.offsetHeight;
-//       const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
-//       target.style.backgroundPosition = `center ${yPosition}px`
-//       heroText.style.transform = `translateY(${-yPosition/2}px)`
-//     }
-//   })
 
-// }
 
-// window.addEventListener('scroll', throttle(calcParallax, 50))
 
-// transform: translate3d(0px, 13.8365%, 0px)
-// Images peices
-// data-speed="-0.25"
-// data-speed="-0.2"
-// data-speed="0.4"
-// data-speed="0.3"
-// data-speed="0.5"
 
-//Headline
-// data-speed="0.1"
 
-const moving = document.querySelectorAll('.tsw-move')
 
-const headLine = document.querySelector('headline')
-const header = document.querySelector('header')
 
-let headerHeight = header.offsetHeight
 
-let scroll = window.scrollY;
 
-moving.forEach((el) => {
-    let speed = el.dataset.speed
 
-    el.computedStyleMap.transform = `translateY(${scroll * speed}px)`
 
-})
 
-headLine.style.opacity = -scroll / (headerHeight / 2) + 1
+
+
+
