c403a7f51434171b3dcbab37ec9da869ec89b2eb
Christian <bournecreative@gmail.com>
2025-01-26 01:25:32 -0600

    rough draft of working solution, scale the hero based on scroll position

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import './style.css'
 import "./styles.scss"
 import throttle from 'lodash.throttle'
 
-const parallaxTarget = document.querySelectorAll('.tsw-parallax')
-console.log(parallaxTarget)
+// console.log(parallaxTarget)
 
 const calculateRangeValue = (oldMin, oldMax, newMin, newMax, oldValue) => {
   const oldRange = oldMax - oldMin;
@@ -19,24 +18,24 @@ const calculateRangeValue = (oldMin, oldMax, newMin, newMax, oldValue) => {
 // This causes the parallax to begin only once half of the image enters viewport
 // ((scrollTop + window.innerHeight) - target.clientHeight / 2) > target.offsetTop)
 
-function calcParallax () {
-  const scrollTop = window.scrollY;
-  parallaxTarget.forEach(target => {
+// function calcParallax () {
+//   const scrollTop = window.scrollY;
+//   parallaxTarget.forEach(target => {
     
-    const heroText = target.nextElementSibling
+//     const heroText = target.nextElementSibling
 
-    if (((scrollTop + window.innerHeight) - target.clientHeight / 2) > target.offsetTop) {
-      const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
-      const oldMax = oldMin + target.offsetHeight;
-      const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
-      target.style.backgroundPosition = `center ${yPosition}px`
-      heroText.style.transform = `translateY(${-yPosition/2}px)`
-    }
-  })
+//     if (((scrollTop + window.innerHeight) - target.clientHeight / 2) > target.offsetTop) {
+//       const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
+//       const oldMax = oldMin + target.offsetHeight;
+//       const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
+//       target.style.backgroundPosition = `center ${yPosition}px`
+//       heroText.style.transform = `translateY(${-yPosition/2}px)`
+//     }
+//   })
 
-}
+// }
 
-window.addEventListener('scroll', throttle(calcParallax, 50))
+// window.addEventListener('scroll', throttle(calcParallax, 50))
 
 // transform: translate3d(0px, 13.8365%, 0px)
 
@@ -44,23 +43,75 @@ window.addEventListener('scroll', throttle(calcParallax, 50))
 // import "./styles.scss";
 
 // // Here we are using intersectional observer to identify when our target is in view
-// window.onload = () => {
-//     // const targetElement = document.querySelector('.test-subject');
-   
-//     // // Intersection will occur when target intersects the middle of the page
-//     // // const observer = new IntersectionObserver(getLocation, { root: null, threshold: 0, rootMargin: '-50% 0% -50% 0%' });
 
-//     // // Intersection will occur when entire target is in view
-//     // const observer = new IntersectionObserver(getLocation, { root: null, threshold: 1 });
-//     // function getLocation(entry) {
-//     //     if (entry[0].isIntersecting) {
-//     //         entry[0].target.style.backgroundColor = "red"
-//     //     } else {
-//     //         entry[0].target.style.backgroundColor = "cyan"
-//     //     }
-//     // }
-//     // observer.observe(targetElement); 
-// }
+    
+    
+    // Intersection will occur when target intersects the middle of the page
+    // const observer = new IntersectionObserver(getLocation, { root: null, threshold: 0, rootMargin: '-50% 0% -50% 0%' });
+
+    // Intersection will occur when entire target is in view
+    // const observer = new IntersectionObserver(getLocation, { root: null, threshold: 0.5  });
+    
+    
+    // function calcParallax (target) {
+    //     console.log(target)
+    //     const scrollTop = window.scrollY;
+    //     const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
+    //     const oldMax = oldMin + target.offsetHeight;
+    //     const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
+    //     // console.log(yPosition)
+    //     // target.style.backgroundPosition = `center ${yPosition}px`
+    //     // heroText.style.transform = `translateY(${-yPosition/2}px)`
+
+    //     // if (((scrollTop + window.innerHeight) - target.clientHeight / 2) > target.offsetTop) {
+    //     //     const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
+    //     //     const oldMax = oldMin + target.offsetHeight;
+    //     //     const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
+    //     //     target.style.backgroundPosition = `center ${yPosition}px`
+    //     //     heroText.style.transform = `translateY(${-yPosition/2}px)`
+    //     // }
+
+    // }
+
+
+
+    // function getLocation(entries) {
+    //     c
+    //     for (const entry of entries) {
+
+    //         var test = (entry) => {
+    //             calcParallax(entry.target)
+    //         }
+    //          if (entry.isIntersecting) {
+               
+    //             window.addEventListener('scroll', test)
+    //         } else {
+    //             window.removeEventListener('scroll', test)
+    //         }
+            
+    //         // console.log(entry)
+    //         // if (entry.isIntersecting) {
+    //         //     entry.target.style.backgroundColor = 'red'
+    //         //     entry.target.style.transform = `translate(-50%, -50%) scale(1)`;
+    //         // } else {
+    //         //     entry.target.style.backgroundColor = 'green'
+    //         //     entry.target.style.transform = `translate(-50%, -50%) scale(0.5)`;
+    //         // }
+    //     }
+
+    //     // console.log(entry)
+    //     // if (entry[0].isIntersecting) {
+    //     //     entry[0].target.style.backgroundColor = "red"
+    //     // } else {
+    //     //     entry[0].target.style.backgroundColor = "cyan"
+    //     // }
+    // }
+
+    // parallaxTarget.forEach((target) => {
+    //     observer.observe(target); 
+    // })
+    
+
 
 // window.addEventListener("scroll", watchTarget)
 
@@ -86,6 +137,65 @@ window.addEventListener('scroll', throttle(calcParallax, 50))
 
 
 
+function setScrollWatcher () {
+    const parallaxTarget = document.querySelectorAll('.tsw-parallax')
+    
+    const calculateRangeValue = (oldMin, oldMax, newMin, newMax, oldValue) => {
+        const oldRange = oldMax - oldMin;
+        const newRange = newMax - newMin;
+
+        return ((oldValue - oldMin) * newRange/oldRange) + newMin
+    }
+
+    parallaxTarget.forEach((target) => {
+
+        const calcParallax = throttle((e) => {
+
+            console.log(((window.innerHeight - target.getBoundingClientRect().top) / window.innerHeight)/1)
+            const scaleValue = ((window.innerHeight - target.getBoundingClientRect().top) / window.innerHeight)
+            // const scrollTop = window.scrollY;
+            // const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
+            // const oldMax = oldMin + target.offsetHeight;
+            // const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
+            // console.log(yPosition)
+            // console.log(yPosition)
+            // console.log(yPosition)
+            // console.log(yPosition)
+            // console.log(yPosition)
+            // target.style.backgroundPosition = `center ${yPosition}px`
+            if (scaleValue < 1) {
+                target.style.transform = `translate(-50%, -50%) scale(${scaleValue * 3}, 1)`;
+            }
+
+            if (scaleValue < 1) {
+                target.style.transform = `translate(-50%, -50%) scale(${scaleValue * 3}, 1)`;
+            }
+            // if (((scrollTop + window.innerHeight) - target.clientHeight / 2) > target.offsetTop) {
+            //     const oldMin = (target.offsetTop < window.innerHeight / 2) ? target.offsetTop : target.offsetTop - window.innerHeight / 2;
+            //     const oldMax = oldMin + target.offsetHeight;
+            //     const yPosition = calculateRangeValue(oldMin, oldMax, 0, -50, scrollTop)
+            //     target.style.backgroundPosition = `center ${yPosition}px`
+            //     heroText.style.transform = `translateY(${-yPosition/2}px)`
+            // }
+        },50)
+        new IntersectionObserver((entries, observer) => {
+            
+            entries.forEach(entry => {
+                // console.log('entry', entry.isIntersecting)
+
+                if (entry.isIntersecting) {
+                    window.addEventListener('scroll', calcParallax, true);
+                } else {
+                    window.removeEventListener('scroll', calcParallax, true);
+                }
+                
+            });
+        }, { root: null, threshold: 0 }).observe(target)
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", setScrollWatcher)
 
 
 
